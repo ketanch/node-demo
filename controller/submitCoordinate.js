929d7efa8faf4fdca50dfe20fd1981cdd5e73546
ketanch <56212393+ketanch@users.noreply.github.com>
2021-01-27 23:47:10 +0530

    Update submitCoordinate.js

--- controller/submitCoordinate.js
+++ controller/submitCoordinate.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const router = express.Router();
-const validate = require('../utilities/validate.js');
-const getcoorCont = require('../controller/getCoordinate.js');
+const {validationResult} = require('express-validator');
+const Coordinate = require('../model/coordinateModule.js');
 
-router.post('/getCoordinates', validate.inputFormat, getcoorCont);
-
-router.get('/getCoordinates', (req, res) => {
-	res.status(200).send("Use POST request to access data.");
-});
-
-module.exports = router;
+module.exports = async (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors : errors.array() });
+	}
+	try {
+		var result = Coordinate.createNew(req.body);
+		res.status(200).send("Successfully Registered.");
+	} catch (error) {
+		res.status(400).send("Some Error occured.");
+	}
+}
